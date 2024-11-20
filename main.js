ff0b831d1ec6e959ad44baf786de301c55b06493
smamby <s.mamby@gmail.com>
2024-11-20 19:06:50 -0300

    triplicando y comentado, listo para entrega

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ function contarVocales(text) {
 console.log(contarVocales("JavaScript"));
 console.log(contarVocales("checklist"));
 console.log(contarVocales("aguantadero"));
-/* Recibo los datos necesarios como parametros y los uso en la formula de area del triangulo*/
+/* Recorro cada letra de la palabra y verifico si esta esta incluida en el array con las 5 vocales, 
+si esta incluida suma 1 a la variable que contabiliza las vocales*/
 
 // EJERCICIO 2
 let arrayNumeros = [1, 2, 3, 4, 5, 8, 6, 3, 3, 2, 9, 1, 6, 7, 8];
@@ -56,9 +57,9 @@ function maxNum(arrayNum) {
 }
 
 console.log(maxNum([1, 2, 3, 4, 5, 8, 6, 3, 3, 2, 9, 1, 6, 7, 8]));
-console.log(maxNum([1, 20, 4, 5, 8, 1, 33, 3, 1, 6, 8]));
+console.log(maxNum([-1, -20, -4, -5, -8, -1, -33, -3, -1, -6, -8]));
 console.log(maxNum([1, 21, 31, 4, 51, 81, 0, -3, 3, -2, 9, 1, 6, 7, 8]));
-/* uso split para separar la cadena por sus espacios metiendo cada parte en un indice de array para luego leer su longitud */
+/* Reduzco el array a un solo numero que represente al mayor de todos, para esto utilizo una funcion ternaria en el callback del reduce */
 
 // EJERCICIO 3
 function calcularMedia(arr) {
@@ -84,7 +85,7 @@ console.log(calcularMedia([10, 20, 30, 40]));
 console.log(calcularMedia([-20, 90, 130, 20]));
 console.log(calcularMedia([1, 1, 1, 40]));
 
-/* Recorro la cadena para atras y cada caracter lo concateno al resultado para ir construyendo una cadena invertida */
+/* Reduzco el array a un solo valor que represente la suma de todos los valores, luego lo divido por la longitud del array y obtengo la media */
 
 // EJERCICIO 4
 function invertirArray(arr) {
@@ -103,14 +104,16 @@ function invertirArray(arr) {
     let response = `Solucion 4: ${invArr}`;  
     consolaPrint(' ');
     consolaPrint(`Ejercicio 4:`);
-    consolaPrint('invertirArray([1,2,3,4,5])');
+    consolaPrint(`invertirArray(${arr})`);
     consolaPrint(response);  
     consolaPrint(' ');
     return response
 }
 
 console.log(invertirArray([1, 2, 3, 4, 5]));
-/*  */
+console.log(invertirArray([10,11,12,'x','y','z']));
+console.log(invertirArray(['a','b','c','d','e']));
+/* Recorro el arra de atras hacia adelante y a cada posicion la empujo a un nuevo array, obteniendo el mismo array pero invertido */
 
 // EJERCICIO 5
 function eliminarDuplicados(arr) {
@@ -127,13 +130,16 @@ function eliminarDuplicados(arr) {
     let response = `Solucion 5: ${newArray}`;
     consolaPrint(' ');
     consolaPrint(`Ejercicio 5:`);
-    consolaPrint('eliminarDuplicados([1, 2, 2, 3, 4, 4, 5]))');
+    consolaPrint(`eliminarDuplicados(${arr}))`);
     consolaPrint(response);  
     consolaPrint(' ');
     return response
 }
 
 console.log(eliminarDuplicados([1, 2, 2, 3, 4, 4, 5]));
+console.log(eliminarDuplicados(['a','a','b','b','b','a','b','a']));
+console.log(eliminarDuplicados(['hola', 'hola', 'hola', 'casa', 'auto', 'auto', 'hola']));
+/* Convierto el array a un set, que por definicion no puede tener elementos repetidos. Luego vuelvo a convertir el set en array */
 
 
 // EJERCICIO 6
@@ -155,15 +161,19 @@ function capitalizarPalabras(input) {
     let response = `Solucion 6: ${newString}`;  
     consolaPrint(' ');
     consolaPrint(`Ejercicio 6:`);
-    consolaPrint('capitalizarPalabras("hola mundo desde javascript")');
+    consolaPrint(`capitalizarPalabras("${input}")`);
     consolaPrint(response);  
     consolaPrint(' ');
     return response
 }
 
 console.log(capitalizarPalabras("hola mundo desde javascript"));
+console.log(capitalizarPalabras("solo se que no se nada"));
+console.log(capitalizarPalabras("sueñan los androides con ovejas electricas"));
 
-/* recibe un imput, lo multiplica, y lo incrusta en una string */
+/* Separo el string por sus espacios y los vuelvo un array con cada palabra utilizando split,
+luego le aplico a cada palabra del array un toUpperCase a el indice 0, para solo afectar a la primer letra, eso lo hago recorriendo todo el array
+pasando por todas las palabras. Cada palabra alterada la guardo sobre el mismo indice haciendo una concatenacion de la primer letra + el resto de la palabra utilizando slice */
 
 // EJERCICIO 7
 function fibonacci(n) {
@@ -189,12 +199,15 @@ function fibonacci(n) {
     let response = `Solucion 7: la lista de numeros de fibonachi de los primeros ${n} numeros son: ${lista}`;  
     consolaPrint(' ');
     consolaPrint(`Ejercicio 7:`);
-    consolaPrint('fibonacci(10)');
+    consolaPrint(`fibonacci(${n})`);
     consolaPrint(response);  
     consolaPrint(' ');
     return response
 }
 
 console.log(fibonacci(10));
+console.log(fibonacci(15));
+console.log(fibonacci(25));
 
-/* recibe un imput, lo multiplica, y lo incrusta en una string */
+/* Uso dos variables de arranque con los primeros dos numeros o y 1, y uso una variable comodin para guardar la suma,
+luego paso el valor 2 al 1 y el 3 al 2 y vuelvo a hacer la suma, repito cuantas veces indique el parametro de entreda n */
